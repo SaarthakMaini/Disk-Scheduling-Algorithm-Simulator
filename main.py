013268d19bb7bc9c18798b83a42f217af42b854e
SaarthakMaini <saarthakmaini@gmail.com>
2023-11-29 18:57:13 +0530

    Add Option To Select Desired Simulations
    Signed-off-by: SaarthakMaini <saarthakmaini@gmail.com>

--- main.py
+++ main.py
@@ -36,27 +36,38 @@ class DiskSchedulerSimulator:
         upper = [r for r in sorted_requests if r > self.initial_position]
         self.sequence_dict['C-LOOK'] = upper + lower
 
-    def visualize_all(self):
+    def visualize_all(self, algorithm_name=None):
         plt.figure(figsize=(12, 6))
-        for algorithm_name, sequence in self.sequence_dict.items():
-            plt.step(range(len(sequence)), sequence, label=algorithm_name)
 
-        plt.title('Disk Scheduling Algorithms Comparison')
+        if algorithm_name:
+            plt.step(range(len(self.sequence_dict[algorithm_name])), self.sequence_dict[algorithm_name], label=algorithm_name)
+            plt.title(f'Disk Scheduling Algorithm: {algorithm_name}')
+        else:
+            for algorithm_name, sequence in self.sequence_dict.items():
+                plt.step(range(len(sequence)), sequence, label=algorithm_name)
+            plt.title('Disk Scheduling Algorithms Comparison')
+
         plt.xlabel('Request Order')
         plt.ylabel('Disk Position')
         plt.legend()
         plt.show()
 
-initial_position = 50
-requests_sequence = [82, 170, 43, 140, 24, 16, 190]
+while True:
+    initial_position = 50
+    requests_sequence = [82, 170, 43, 140, 24, 16, 190]
 
-simulator = DiskSchedulerSimulator(initial_position, requests_sequence)
+    simulator = DiskSchedulerSimulator(initial_position, requests_sequence)
 
-simulator.fcfs()
-simulator.sstf()
-simulator.scan()
-simulator.c_scan()
-simulator.look()
-simulator.c_look()
+    simulator.fcfs()
+    simulator.sstf()
+    simulator.scan()
+    simulator.c_scan()
+    simulator.look()
+    simulator.c_look()
 
-simulator.visualize_all()
+    algorithm_name = input("Enter the algorithm name to visualize (or type 'x' to exit): ").upper()
+
+    if algorithm_name == 'X':
+        break  
+    else:
+        simulator.visualize_all(algorithm_name)
